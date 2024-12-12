7387cbe0525a0724cb76996184262fd2e6466f83
SANGWOO PARK <prophet0915@gmail.com>
2024-12-12 19:12:07 +0900

    display time according to region

---
+++ lib/dangple_time_picker.dart
@@ -0,0 +1,8 @@
+library dangple_time_picker;
+
+import 'dart:math' as math;
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/scheduler.dart';
+
+part 'src/cupertino_korean_time_picker.dart';
---
+++ test/dangple_time_picker_test.dart
@@ -0,0 +1,9 @@
+import 'package:dangple_time_picker/dangple_time_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() => Scaffold(
+      body: KoCupertinoDatePicker(
+        mode: CupertinoDatePickerMode.time,
+        onDateTimeChanged: (value) {},
+      ),
+    );
